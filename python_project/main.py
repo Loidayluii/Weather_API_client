be11d9ae0220de8b20c13254a580161b22c4cbc8
Le Quoc Loi <quocloihoak34ctn@gmail.com>
2026-07-12 23:51:19 +0700

    Update main.py

--- python_project/main.py
+++ python_project/main.py
@@ -8,7 +8,33 @@ from PyQt5.QtCore import Qt
 class WeatherApp(QWidget):
  def __init_(self): 
         super().__init__()
+        self.city_label = QLabel("Enter city name: " , self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather" , self)
+        self.temperature_label =  QLabel("28 do C",self)
+        self.emoji_label = QLabel("*" , self) 
+        self.description_label = QLabel("Sunny",self)
+        self.initUI()
+        
+        def initUI(self):
+            self.setWindowTitle("Weather App")
 
+            vbox = QVBoxLayout();
+        
+            vbox.addWidget(self.city_lable)
+            vbox.addWidget(self.city_input)
+            vbox.addWidget(self.get_weather_button)
+            vbox.addWidget(self.temperature_label)
+            vbox.addWidget(self.emoji_label)
+            vbox.addWidget(self.description_label)
+
+        self.setLayout (box)
+
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
         if __name__ == "__main__":
             app = QApplication(sys.argv)
             weather_app = WeatherApp()
